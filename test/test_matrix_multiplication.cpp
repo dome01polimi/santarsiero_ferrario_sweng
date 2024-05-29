d83fed5024cb386d55a8185bb65350c86e2e28b4
Dome <domenico.santarsiero@mail.polimi.it>
2024-05-29 11:56:09 +0200

    first commit

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -28,7 +28,68 @@ TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
 
-int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+TEST(MatrixMultiplicationTest, TestMultiplyMatrices_ZeroMatrix) {
+    std::vector<std::vector<int>> A = {
+        {0, 0, 0},
+        {0, 0, 0}
+    };
+    std::vector<std::vector<int>> B = {
+        {1, 2},
+        {3, 4},
+        {5, 6}
+    };
+    std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
+    multiplyMatrices(A, B, C, 2, 3, 2);
+
+    std::vector<std::vector<int>> expected = {
+        {0, 0},
+        {0, 0}
+    };
+
+    ASSERT_EQ(C, expected) << "Matrix multiplication test failed!";
+}
+
+TEST(MatrixMultiplicationTest, TestMultiplyMatrices_IdentityMatrix) {
+    std::vector<std::vector<int>> A = {
+        {1, 0},
+        {0, 1}
+    };
+    std::vector<std::vector<int>> B = {
+        {2, 3},
+        {4, 5}
+    };
+    std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
+    multiplyMatrices(A, B, C, 2, 2, 2);
+
+    std::vector<std::vector<int>> expected = {
+        {2, 3},
+        {4, 5}
+    };
+
+    ASSERT_EQ(C, expected) << "Matrix multiplication test failed!";
+}
+
+//test on diagonal matrix
+TEST(MatrixMultiplicationTest, TestMultiplyMatrices_DiagonalMatrix) {
+    std::vector<std::vector<int>> A = {
+        {2, 0, 0},
+        {0, 3, 0},
+        {0, 0, 4}
+    };
+    std::vector<std::vector<int>> B = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    std::vector<std::vector<int>> C(3, std::vector<int>(3, 0));
+    multiplyMatrices(A, B, C, 3, 3, 3);
+    std::vector<std::vector<int>> expected = {
+        {2, 4, 6},
+        {12, 15, 18},
+        {28, 32, 36}
+    };
+    ASSERT_EQ(C, expected) << "Matrix multiplication test failed!";
+
+//test on inverse matrix
+
 }
